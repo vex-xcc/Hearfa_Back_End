77fe0f0d20272a721e33f2e047a8909a2e108c74
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-04-08 02:09:38 +0300

    Find all service depend on userId and if the ServiceState is closed

--- app/routes/service.js
+++ app/routes/service.js
@@ -3,8 +3,30 @@ const router = express.Router();
 const Customer = require('../models/Customer');
 const Service = require('../models/Service');
 
+
+
+//---------------The GET router-------------------
+//Find all service depend on userId and if the ServiceState is closed
+router.get('/api/service/closed/:WorkerId', (req, res) => {
+    Customer.findById(req.params.WorkerId, (error, foundWorker) => {
+      Service.find({})
+        .where("ServiceState").in('closed')
+        .populate('ServicesEmp')
+  
+        .exec((err, Customer) => {
+          if (err) {
+            res.status(500).send(err);
+            return;
+          }
+          console.log(`found and populated all : ${Customer}`);
+          res.status(200).json(Customer);
+        })
+    });
+  });
+
+
 //---------------The PATCH router-------------------
-//-------------Update Service by Service Id-------------------
+//Update Service by Service Id
 router.patch('/api/UpdateService/:ServiceId', (req, res) => {
     Service.findById(req.params.ServiceId, async (error, foundService) => {
       try {
@@ -16,7 +38,12 @@ router.patch('/api/UpdateService/:ServiceId', (req, res) => {
     });
   });
 
-//-------------Delete Service by Service Id-------------------
+
+
+
+//---------------The PATCH router-------------------  
+
+//Delete Service by Service Id
 router.delete('/api/DeleteService/:ServiceId', (req, res) => {
     Service.findById(req.params.ServiceId, async (error, foundService) => {
       try {
