b1655854c2bc2089ff2ed8738da5c76319e9895b
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-04-08 02:06:22 +0300

    add get route to delete Service by Service Id

--- app/routes/service.js
+++ app/routes/service.js
@@ -3,6 +3,18 @@ const router = express.Router();
 const Customer = require('../models/Customer');
 const Service = require('../models/Service');
 
+//-------------Update Service by Service Id-------------------
+router.patch('/api/UpdateService/:ServiceId', (req, res) => {
+    Service.findById(req.params.ServiceId, async (error, foundService) => {
+      try {
+        await foundService.update(req.body);
+        res.status(200).json(req.body);
+      } catch (error) {
+        res.status(404).json(error);
+      }
+    });
+  });
+  
 //-------------Delete Service by Service Id-------------------
 router.delete('/api/DeleteService/:ServiceId', (req, res) => {
     Service.findById(req.params.ServiceId, async (error, foundService) => {
