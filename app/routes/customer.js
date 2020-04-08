749ddb4c564b87ec8bbf8b400f8ef22a245d3bc6
vex-xcc <vex.xcc@gmail.com>
2020-04-08 12:06:13 +0300

    add rout to register a new user

--- app/routes/customer.js
+++ app/routes/customer.js
@@ -4,8 +4,44 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const Customer = require('../models/Customer');
 const config = require('../../config/db');
-
 require("dotenv").config();
+
+
+//----------------------- All Post Methods ----------------------------\\
+//this rout response to register a new user
+router.post('/api/customer/register', (req, res) => {
+  const { FullName, Username, Email, password, Phone,Worker,UserType } = req.body;
+  const customer = {}
+  customer.FullName = FullName,
+      customer.Username = Username,
+      customer.Email = Email,
+      customer.Phone = Phone,
+      customer.password = password,
+      customer.Worker = Worker,
+      customer.UserType = UserType
+  let newCustomer = new Customer(customer)
+
+  Customer.addUser(newCustomer, (err, customer) => {
+      if (err) {
+          let message = "";
+          if (err.errors.Username) message = "Username is already exist. ";
+           if (err.errors.Email) message = "Email already exists.";
+           if (err.errors.Username ) message = "Please Make sure to fill a valid Username.";
+          if (err.errors.Email ) message = "Please Make sure to fill a valid Email.";
+          if (err.errors.password ) message = "Please Make sure to fill a valid password.";
+          if (err.errors.FullName) message = "Please Make sure to fill a valid FullName.";
+          return res.json({
+              success: false,
+              message
+          });
+      } else {
+          return res.json({
+              success: true,
+              message: "User registration is successful."
+          });
+      }
+  });
+});
 
 
 //---------------The GET router-------------------
@@ -18,6 +54,7 @@ router.get('/api/customer/:UserId', (req, res) => {
       console.log('Services',User);
       res.send(User.Username)
         });
-
       });
+
+
 module.exports = router;
