82f2bb9485111cc9025471801e0a70a4023f6ff6
vex-xcc <vex.xcc@gmail.com>
2020-04-08 10:58:03 +0300

    add create service embedded in customer requst  to store new Service in memory with data from request body and find Customer in db by id and add new service

--- app/routes/service.js
+++ app/routes/service.js
@@ -2,10 +2,35 @@ const express = require('express');
 const router = express.Router();
 const Customer = require('../models/Customer');
 const Service = require('../models/Service');
+//---------------All Post Methods-------------------
 
+//-------------create service embedded in customer-------------------
+router.post('/api/:customerId', (req, res) => {
+  // store new Service in memory with data from request body
+  const { ServiceType, ServiceDescription, ServiceState,ServicePrice } = req.body
+  let service = {}
+  service.ServiceDescription = ServiceDescription,
+    service.ServiceState = ServiceState,
+    service.ServicesEmp = req.params.customerId,
+    service.ServiceType = ServiceType,
+    service.ServicePrice = ServicePrice
+  const savedService = new Service(service)
+  savedService.save()
+  // find Customer in db by id and add new service
+  Customer.findById(req.params.customerId, async (error, foundCustomer) => {
+    try {
+      await foundCustomer.RequestService.push(savedService);
+      foundCustomer.save()
+      res.status(200).json(savedService);
+    }
+    catch (error) {
+      res.status(404).json(error);
+    }
+  });
+});
 
+//--------------- All GET Methods -------------------
 
-//---------------The GET router-------------------
 //Find all service depend on userId and if the ServiceState is closed
 router.get('/api/service/closed/:WorkerId', (req, res) => {
     Customer.findById(req.params.WorkerId, (error, foundWorker) => {
@@ -25,7 +50,9 @@ router.get('/api/service/closed/:WorkerId', (req, res) => {
   });
 
 
-//---------------The PATCH router-------------------
+//--------------- All Patch Methods -------------------
+
+
 //Update Service by Service Id
 router.patch('/api/UpdateService/:ServiceId', (req, res) => {
     Service.findById(req.params.ServiceId, async (error, foundService) => {
@@ -41,7 +68,9 @@ router.patch('/api/UpdateService/:ServiceId', (req, res) => {
 
 
 
-//---------------The Delete router-------------------  
+//--------------- All Delete Methods ------------------- 
+
+
 //Delete Service by Service Id
 router.delete('/api/DeleteService/:ServiceId', (req, res) => {
     Service.findById(req.params.ServiceId, async (error, foundService) => {
