78f1a08e75f56e70f64e1add3658cc649a86ac8b
vex-xcc <vex.xcc@gmail.com>
2020-04-09 08:17:20 +0300

    chane the cors

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const app = express()
 
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}`}))
+app.use(cors({ origin: `https://vex-xcc.github.io/hearfa_front-end` || `http://localhost:${reactPort}`}))
 
 // define port for API to run on
 const port = process.env.PORT || expressPort
